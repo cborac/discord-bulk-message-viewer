4b3b3ee3a08c83797e40389a6db3fdf553599264
Sardonyx <canboraciner@gmail.com>
2020-07-18 17:41:04 +0300

    Better Support for uploading files directly and custom emotes can be used

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ https://discord-bulk-message-viewer.sardonyx.me?data=ENCODEDURICOMPONENTHERE
 [Example](https://sardonyx.me/discord-bulk-message-viewer/?data=%5B%0D%0A%20%20%20%20%20%7B%0D%0A%20%20%20%20%20%20%20%20%20%20%22author_id%22%3A%20%22295222772546928641%22%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22avatar_hash%22%3A%20%222ba235580297f757f3cd4bf188a94385%22%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22hexcolor%22%3A%20%22B99CBA%22%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22timestamp%22%3A%201594744316220%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22author_nickname%22%3A%20%22Sardonyx%22%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22content%22%3A%20%22Hello%20World!%22%0D%0A%20%20%20%20%20%7D%2C%0D%0A%20%20%20%20%20%7B%0D%0A%20%20%20%20%20%20%20%20%20%20%22author_id%22%3A%20%22295222772546928641%22%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22avatar_hash%22%3A%20%222ba235580297f757f3cd4bf188a94385%22%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22hexcolor%22%3A%20%22B99CBA%22%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22timestamp%22%3A%201594744316220%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22author_nickname%22%3A%20%22Sardonyx%22%2C%0D%0A%20%20%20%20%20%20%20%20%20%20%22content%22%3A%20%22I%20said%20hello%3F%22%0D%0A%20%20%20%20%20%7D%0D%0A%5D)<br>
 or you can use discord attachments
 ```
-https://sardonyx.me/discord-bulk-message-viewer?channel=Channel_ID&attachment=Attachment_ID&name=ATTACHMENT.name
+https://sardonyx.me/discord-bulk-message-viewer?channel=(channel id)&attachment=(attachment id)&name=(url encoded attachment name)
 ```
 [Example](https://sardonyx.me/discord-bulk-message-viewer?channel=716972875852283904&attachment=732679506858475600&name=bulkmsg.json)
 
@@ -42,3 +42,9 @@ channel.send('Bulk messages:', new MessageAttachment(Buffer.from(JSON.stringify(
      msg.channel.send(`Preview: https://sardonyx.me/discord-bulk-message-viewer?channel=${msg.channel.id}&attachment=${msg.attachments.first().id}&name=${encodeURIComponent(msg.attachments.first().name)}`)
 })
 ```
+
+
+#### Credits ig
+- Emojis: [Twemoji](https://github.com/twitter/twemoji)
+- Time: [Moment.js](https://momentjs.com/)
+- Markdown: [Simple-Markdown](https://github.com/Khan/simple-markdown)
--- javascript.js
+++ javascript.js
@@ -12,13 +12,81 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-function createMessage({ author_id, avatar_hash, hexcolor, timestamp, author_nickname, content }) {
+
+const usercache = []
+
+function createElementFromHTML(htmlString) {
+     const div = document.createElement('div');
+     div.innerHTML = htmlString.trim();
+     return div.firstChild; 
+   }
+
+/**
+ * 
+ * @param {*} param0 
+ * @param {*[]} original 
+ */
+
+async function createMessage({ author_id, avatar_hash, hexcolor, timestamp, author_nickname, content }, original, i) {
+
+     /**
+      * @type {string}
+      */
+     let trueContent = content.split('\n').join('<br>')
+
+     author_nickname = author_nickname.split('<').join('&lt;').split('>').join('&gt;')
+     content = content.split('<').join('&lt;').split('>').join('&gt;')
+
+     const rules = SimpleMarkdown.defaultRules;
+     rules.url = {
+          /**
+           * @param {Node} node
+           */
+          html: (node, output) => node.content
+     }
+
+     const parser = SimpleMarkdown.parserFor(rules);
+
+     const mentions = trueContent.match(/<@![0-9]+>/g)
+
+     if (mentions) for (const mention of mentions) {
+          const id = mention.substring(3, mention.length - 1)
+          const cached = original.find(x => x.author_id === id) || usercache.find(x => x.id === id)
+          if (cached) {
+               trueContent = trueContent.replace(/<@![0-9]+>/g, `<span class="mention">@${cached.author_nickname}</span>`)
+          }else {
+               const body = await fetch('https://discord-number-bot.herokuapp.com/?id=' + id).then(x => x.json())
+
+               body.author_nickame = body.name
+
+               usercache.push(body)
+
+               trueContent = trueContent.replace(/<@![0-9]+>/g, `<span class="mention">@${body.name}</span>`)
+          }
+     }
+
+     const customEmotes = trueContent.match(/<a?:(\w+):\d+>|:(\w+):/gi)
+
+     if (customEmotes) for (let i2 = 0; i2 < customEmotes.length; i2++) {
+          const parsedEmoji = customEmotes[i2].match(/^<a?:(\w+):(\d+)>$/)
+
+
+          trueContent = trueContent.replace(/<a?:(\w+):\d+>|:(\w+):/i, `<img draggable="false" class="emoji" alt="${parsedEmoji[1]}" src="https://cdn.discordapp.com/emojis/${parsedEmoji[2]}.${parsedEmoji[0].startsWith('<a:') ? 'gif' : 'png'}">`)
+     }
+
+     const out = SimpleMarkdown.defaultHtmlOutput(parser(trueContent), { inline: true })
+
+     trueContent = createElementFromHTML(out).innerHTML.split('&lt;img').join('<img').split('"&gt;').join('">')
 
      const msg = document.createElement('div')
      msg.className = "message"
 
      const right = document.createElement('div')
      right.className = "right"
+
+     const msgContent = document.createElement('div')
+     msgContent.innerHTML = trueContent
+     msgContent.className = 'messageContent'
 
      const spans = [ document.createElement('span'), document.createElement('span') ]
      spans[0].className = "nick"
@@ -27,9 +95,7 @@ function createMessage({ author_id, avatar_hash, hexcolor, timestamp, author_nic
      spans[0].style.color = `#${hexcolor}`
      spans[1].innerHTML = moment(new Date(timestamp)).calendar()
 
-     const msgContent = document.createElement('div')
-     msgContent.innerHTML = content
-     msgContent.className = 'messageContent'
+
 
      right.append(spans[0])
      right.append(spans[1])
@@ -42,16 +108,19 @@ function createMessage({ author_id, avatar_hash, hexcolor, timestamp, author_nic
      msg.append(avatar)
      msg.append(right)
 
+     if (i === 0) msg.style['margin-top'] = "0px"
+
      return msg
 }
 
-function load(data){
+async function load(data){
      if (document.getElementsByClassName('message').length !== 0) for (let i = document.getElementsByClassName('message').length; i === 0; i--) {
           document.getElementsByClassName('message')[i].remove()
      }
-     data.sort((a, b) => a - b).forEach(element => {
-          document.body.append(createMessage(element))
-     });
+     data = data.sort((a, b) => a.timestamp - b.timestamp)
+     for (let i = 0; i < data.length; i++)  document.body.append(await createMessage(data[i], data, i))
+     twemoji.parse(document.body);
+     document.getElementById('loader').style.visibility = 'hidden'
 }
 
 const query = new URLSearchParams(location.search)
@@ -66,7 +135,10 @@ const name = query.get('name')
  * @param {HTMLInputElement} input
  */
 async function fileUpload(input){
-   location.search = '?data=' + encodeURIComponent(await input.files[0].text())
+     console.log(await input.files[0].text())
+     document.getElementById('loader').style.visibility = 'visible'
+     document.getElementById('input').remove()
+     load(JSON.parse(await input.files[0].text()))
 };
 
 (async function() {
@@ -75,12 +147,12 @@ async function fileUpload(input){
           const res = await fetch(`https://cors-anywhere.herokuapp.com/https://cdn.discordapp.com/attachments/${channel}/${attachment}/${name}`)
           const body = await res.text()
           load(JSON.parse(body))
-          document.getElementById('loader').style.visibility = 'hidden'
      }
      else if (data === null) {
           const input = document.createElement('input')
           input.type = 'file'
           input.accept = '.json'
+          input.id = 'input'
           input.setAttribute('onchange', 'fileUpload(this)')
           document.body.append(input)
      }
